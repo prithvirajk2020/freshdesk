7314c8abb8e2bcc88a65532b74cd147d639f459b
prithvirajk2020 <prithvirajk2020@gmail.com>
2026-01-30 14:51:23 +0530

    third commit

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
+require('dotenv').config();
 const app = express();
 
 // ================================
